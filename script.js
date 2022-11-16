16e72ad0f864955b163a4cb8dfd9dd6ec0166ac2
Andrew Prinn <andrewprinn@gmail.com>
2022-11-16 17:42:28 -0500

    fixed errors

--- script.js
+++ script.js
@@ -8,28 +8,24 @@ class Book {
     this.read = read;
   }
 }
-  
-const book = new Book (prompt("Title"), prompt("Author"), prompt("Pages"), prompt("Read?"))
 
 function addBookToLibrary() {
   myLibrary.push(book);
 }
 
-
-
-  document.getElementById("title").innerHTML= book.title; 
-  document.getElementById("author").innerHTML= book.author; 
-
-  const container = document.getElementById("container");
+const container = document.getElementById("container");
 
 document.querySelector('button').addEventListener("click", formFunction)
 function formFunction(){
   let card = document.createElement("div");
-  card.className = "card";
-  let title = document.createElement("title");
-  let author =  document.createElement("author");
-  card.appendChild(title);
-  card.appendChild(author);
+    card.className = "card";
+      container.appendChild(card);
+  let title = document.createElement("div");
+    title.id = "title";
+      card.appendChild(title);
+  let author =  document.createElement("div");
+    author.id = "author";
+      card.appendChild(author);
   const book = new Book (prompt("Title"), prompt("Author"), prompt("Pages"), prompt("Read?"))
   document.getElementById("title").innerHTML= book.title; 
   document.getElementById("author").innerHTML= book.author; 
