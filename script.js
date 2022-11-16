50010a3e0269e133d49b3626d79f64235e69c281
Andrew Prinn <andrewprinn@gmail.com>
2022-11-15 21:20:31 -0500

    can prompt for book details and display a card with info

--- script.js
+++ script.js
@@ -6,9 +6,6 @@ class Book {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.inform = function info() {
-      return information = (title + " by " + author + ", " + pages + " pages. Read: " + read);
-    };
   }
 }
   
@@ -18,5 +15,6 @@ myLibrary.push(book);
 console.log(myLibrary);
 
 
-let info = book.title  + " by " + book.author + ", " + book.pages + " pages. Read: " + book.read;
-document.getElementById("card").innerHTML= info 
+//let info = book.title  + " by " + book.author + ", " + book.pages + " pages. Read: " + book.read;
+document.getElementById("title").innerHTML= book.title; 
+document.getElementById("author").innerHTML= book.author; 
