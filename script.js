e5551dfd14e8445d33a8157e5a16a1a88d0dbb4c
Andrew Prinn <andrewprinn@gmail.com>
2022-11-28 11:12:31 -0500

    can display multiple books, but the first book gets duplicated.

--- script.js
+++ script.js
@@ -19,23 +19,33 @@ document.querySelector('button').addEventListener("click", formFunction)
 function formFunction(){
   const book = new Book (prompt("Title"), prompt("Author"), prompt("Pages"), prompt("Read?"));
   addBookToLibrary(book);
-  displayBook(book);
+  console.log(myLibrary);
+  cardDisplay();
 }
 
-myLibrary.forEach(displayBook);
-let i = 0;
-function displayBook(){
-  let card = document.createElement("div");
-    card.className = "card";
+function cardDisplay(){
+  for (i = 0; i < myLibrary.length; i++){
+    let card = document.createElement("div");
+      card.className="card";
       container.appendChild(card);
-  let title = document.createElement("div");
-    title.id = "title";
+    let title = document.createElement("div");
+      title.classname = "title";
+      title.innerHTML = myLibrary[i].title;
       card.appendChild(title);
-  let author =  document.createElement("div");
-    author.id = "author";
+    let author = document.createElement("div");
+      author.classname = "author";
+      author.innerHTML = myLibrary[i].author;
       card.appendChild(author);
-  document.getElementById("title").innerHTML= myLibrary[i].title; 
-  document.getElementById("author").innerHTML= myLibrary[i].author;
-  i++; 
+    let pages = document.createElement("div");
+      pages.classname = "pages";
+      pages.innerHTML = myLibrary[i].pages; 
+      card.appendChild(pages);
+    let read = document.createElement("div");
+      read.classname = "pages";
+      read.innerHTML = myLibrary[i].pages; 
+      card.appendChild(read);
+  }
 }
 
+
+console.log(myLibrary);
